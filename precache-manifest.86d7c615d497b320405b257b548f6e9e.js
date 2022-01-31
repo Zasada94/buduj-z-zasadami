c4faf071ff48ada238349d682910246f471d1ab6
Zasada94 <pawelzasada2@wp.pl>
2022-01-31 16:36:31 +0100

    Updates

---
+++ precache-manifest.86d7c615d497b320405b257b548f6e9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "247e5bd9bea9aa49d878",
+    "url": "/buduj-z-zasadami/static/css/main.8db36101.chunk.css"
+  },
+  {
+    "revision": "247e5bd9bea9aa49d878",
+    "url": "/buduj-z-zasadami/static/js/main.247e5bd9.chunk.js"
+  },
+  {
+    "revision": "981587391077a106d1f0",
+    "url": "/buduj-z-zasadami/static/js/runtime~main.98158739.js"
+  },
+  {
+    "revision": "8c3470d849286fee9339",
+    "url": "/buduj-z-zasadami/static/js/2.8c3470d8.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/buduj-z-zasadami/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/buduj-z-zasadami/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/buduj-z-zasadami/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "88ee58629e4044474a42ef2a5e0b0497",
+    "url": "/buduj-z-zasadami/index.html"
+  }
+];
