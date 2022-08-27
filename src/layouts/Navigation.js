5da8524d1a878e5dbc7561a6d77a72c687aef639
Zasada94 <pawelzasada2@wp.pl>
2022-08-27 15:04:57 +0200

    aside on click added mobile v

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Navigation.css";
+import { useState } from "react";
 
 const list = [
-  { name: "start", path: "/", exact: true },
-  { name: "oferta", path: "/products" },
-  { name: "kontakt", path: "/contact" },
-  { name: "panel admina", path: "/admin" },
+	{ name: "start", path: "/", exact: true },
+	{ name: "oferta", path: "/products" },
+	{ name: "kontakt", path: "/contact" },
+	{ name: "panel admina", path: "/login" },
 ];
 
 const menu = list.map((item) => (
-  <li key={item.name}>
-    <NavLink to={item.path} exact={item.exact ? item.exact : false}>
-      {item.name}
-    </NavLink>
-  </li>
+	<li key={item.name}>
+		<NavLink to={item.path} exact={item.exact ? item.exact : false}>
+			{item.name}
+		</NavLink>
+	</li>
 ));
 const Navigation = () => {
-  return (
-    <nav className="main">
-      <ul>{menu}</ul>
-    </nav>
-  );
+	const [show, setShow] = React.useState();
+	return (
+		<div className={show ? "show navWrapper" : "navWrapper"}>
+			<nav className="main">
+				<ul>{menu}</ul>
+			</nav>
+			<div className="navButton">
+				<button onClick={() => setShow(!show)}>
+					<i class="fa-solid fa-arrow-right"></i>
+					<i class="fa-solid fa-arrow-left"></i>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Navigation;
